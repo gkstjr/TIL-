f966a7665c7e89809ae8c8f9f0161ca25e81e491
gkstjr <gkstjr8332@naver.com>
2022-02-19 20:58:51 +0900

    ㄹ

--- src/javaStudy/for01.java
+++ src/javaStudy/for01.java
@@ -1,6 +1,15 @@
-package java;
-//tilK
+package javaStudy;
+//배열 연습
 public class for01 {
-//
+	public static void main(String[] args) {
+		String[] a = new String[3];
+		a[0] = "안녕";
+		a[1] = "나는 ";
+		a[2] = "김한석이라고해";
+		
+		System.out.print(a[0] + " ");
+		System.out.print(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a.length);
+	}
 }
-//
