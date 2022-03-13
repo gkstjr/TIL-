864e8fa145356803cd7499c4b93a710fed354d69
gkstjr <gkstjr8332@naver.com>
2022-03-13 20:10:37 +0900

    Stack과 Queue 연습

--- src/ch11/Teast1.java
+++ src/ch11/Teast1.java
@@ -1,10 +1,29 @@
 package ch11;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Teast1 {
 	public static void main(String[] args) {
-		ArrayList list1 = new ArrayList();
+		Stack st = new Stack();
+		Queue q = new LinkedList();
 		
+		st.push("0");
+		st.push("1");
+		st.push("2");
+		
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+		
+		System.out.println("= Stack =");
+		while(!st.empty()) {
+			System.out.println(st.pop());
+		}
+		System.out.println("= queue = ");
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
 	}
 }
