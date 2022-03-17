4f416f359198ebbe7d22e3485df41fa157e645e4
gkstjr <gkstjr8332@naver.com>
2022-03-17 23:10:56 +0900

    ㅎ

--- src/ch14/Ex16.java
+++ src/ch14/Ex16.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public class Ex16 {
 public static void main(String[] args) {
 //	int[] arr = null;
-	int[] arr = new int[0];
+	int[] arr = new int[0];  
 	System.out.println("arr.length = "+arr.length);
 	
 //	Optional<String> opt = null; 바람직하지 않음
