8e7809d84b6a75e222d3c9d950916f11c9f48218
gkstjr <gkstjr8332@naver.com>
2022-03-22 00:38:58 +0900

    자바의정석 복습

---
+++ src/javaStudy/Test03.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class Test03 {
+public static void main(String[] args) {
+	int num = -1;
+	System.out.println(num<0 ? "음수":(num==0? num:"양수"));
+}
+}
---
+++ src/javaStudy/Test04.java
@@ -0,0 +1,9 @@
+package javaStudy;
+
+public class Test04 {
+	public static void main(String[] args) {
+		int fahrenheit = 100;
+		float celcius = (int)((5f/9*(fahrenheit-32))*100+0.5f)/100f;
+		System.out.println(celcius);
+	}
+}
