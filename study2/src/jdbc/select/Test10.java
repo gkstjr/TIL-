d86b2af183f03b9c8fc884e89acd6853cda5bad4
gkstjr <gkstjr8332@naver.com>
2022-03-31 23:52:45 +0900

    .

--- study2/src/jdbc/select/Test10.java
+++ study2/src/jdbc/select/Test10.java
@@ -2,7 +2,7 @@ package jdbc.select;
 
 import jdbc.beans.OlympicDao;
 import jdbc.beans.OlympicDto;
-
+  
 public class Test10 {
 	public static void main(String[] args) throws Exception {
 		int rank = 99;
