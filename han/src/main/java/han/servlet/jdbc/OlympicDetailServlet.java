35124cc4b429bf9acc21dd6775f9d6ba69bf5dcb
gkstjr <gkstjr8332@naver.com>
2022-04-02 16:13:05 +0900

    servlet 업데이트구문(오타 찾기에 시간엄청쓴다)

---
+++ han/src/main/java/han/servlet/jdbc/OlympicDetailServlet.java
@@ -0,0 +1,38 @@
+package han.servlet.jdbc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import han.beans.OlympicDao;
+import han.beans.OlympicDto;
+
+@WebServlet(urlPatterns= "/olympic/detail.kh")
+public class OlympicDetailServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			int rank = Integer.parseInt(req.getParameter("rank"));
+			
+			OlympicDao olympicDao = new OlympicDao();
+			OlympicDto olympicDto = olympicDao.selectOne(rank);
+			
+			
+			resp.setContentType("text/plain; charset=UTF-8");
+			resp.getWriter().println(olympicDto.getRank());
+			resp.getWriter().println(olympicDto.getName());
+			resp.getWriter().println(olympicDto.getGold());
+			resp.getWriter().println(olympicDto.getSilver());
+			resp.getWriter().println(olympicDto.getBronze());
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
