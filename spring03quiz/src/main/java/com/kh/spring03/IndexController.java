70ee188578461ae9d6f02fe449803c794b858487
gkstjr <gkstjr8332@naver.com>
2022-05-28 21:50:22 +0900

    개인복습용 spring03프로젝트 생성

---
+++ spring03quiz/src/main/java/com/kh/spring03/IndexController.java
@@ -0,0 +1,18 @@
+package com.kh.spring03;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/WEB-INF/views/index.jsp");
+		return mv;
+	}
+
+}
