65e32b002f4cce1b228efa490b2d65a8ea5ba51a
kimhanseok <gkstjr8332@naver.com>
2023-02-10 17:46:23 +0900

    배열 알고리즘

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -3,6 +3,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 
@@ -11,23 +15,25 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int n = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+		for(int i =0; i<n; i++) {
+			st  = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(st.nextToken());
+			int[] arr = new int[first];
+			double sum = 0;
+			for(int j =0; j<first; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+				sum += arr[j];
+			}
+			double cnt = 0;
+			double avg = sum / (double)first;
 			
-			bw.write("Case #" + (i+1) +": " + a + " + " + b + " = "  + (a + b) + "\n");
+			for(int j=0; j<first; j++) {
+				if(arr[j] > avg) cnt++;
+			}
+			System.out.printf("%.3f%%\n",(cnt /first) *100);
 		}
-		
 		br.close();
-		bw.flush();
-		bw.close();
-	
-	
 	}
 }	
