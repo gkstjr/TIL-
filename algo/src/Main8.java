7d0d7c00f278ac19d659d2115da32a2f49b11eb9
kimhanseok <gkstjr8332@naver.com>
2023-02-10 09:34:27 +0900

    배열 알고리즘 공

---
+++ algo/src/Main8.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main8 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		   StringBuilder sb = new StringBuilder();
+		   
+		   int n = Integer.parseInt(br.readLine());
+		   int firstNum = n;
+		   int T = 0;
+		   int count =0;
+		   while(true) {
+			   n = ((n % 10) *10) + (((n/ 10) + (n % 10)) % 10);
+			   count++;
+			   if(firstNum == n) break;
+		   }
+		   
+		   sb.append(count).append("\n");
+		   System.out.println(sb);
+		 
+		   
+	}
+}	
