89a5a7552b6cca7bad868fc23a2023c42dccbae9
kimhanseok <gkstjr8332@naver.com>
2023-02-12 17:05:49 +0900

    문자열 알고리즘 1

---
+++ algo/src/Main17.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main17 {
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int n = br.readLine().charAt(0);
+			System.out.println(n);
+	}
+}	
