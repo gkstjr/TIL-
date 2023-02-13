944087bffbb20b4711ca6906c8114b8520aa3c35
kimhanseok <gkstjr8332@naver.com>
2023-02-13 09:07:26 +0900

    문자열 백준1

---
+++ algo/src/Main19.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main19 {
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = -1;
+		}
+		
+		String s = br.readLine();
+		
+		for(int i =0 ; i< s.length(); i++	 ) {
+			char ch = s.charAt(i);
+			
+			if(arr[ch - 'a'] == -1) {
+				arr[ch - 'a'] = i;				
+			}
+		}
+		
+		for(int x : arr) {
+			System.out.print(x +" ");
+		}
+	}
+}	
