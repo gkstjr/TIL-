cd16c4a83df36a80af0b8b69b6e08a5122e0faa5
kimhanseok <gkstjr8332@naver.com>
2023-02-15 14:43:16 +0900

    수학 문제1

---
+++ algo/src/Main24.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main24 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		long n = 1;
+		while(true) {
+			if(a + (b * n) < c * n)	 break;
+			if(n == 2100000001) {
+				n = -1;
+				break;
+			}
+			
+			
+			n++;
+		}
+		
+		System.out.println(n);
+	}
+}	
