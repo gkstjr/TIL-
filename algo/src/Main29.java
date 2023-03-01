65e398dcbe1d3def6965f4cdc2ee9b4a9fc19a0f
gkstjr <gkstjr8332@naver.com>
2023-03-01 11:30:04 +0900

    자바 형변환 확인!

---
+++ algo/src/Main29.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main29 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	
+		String a = "한석";
+		int b = 3;
+		
+//		System.out.println(a / b); 	컴파일 오류1
+//		System.out.println(a % b); 컴파일 오류2
+		System.out.println(a + b );
+//		System.out.println(a - b);컴파일 오류3
+	}	
+}	
