9b0a5fbc1a896da3344406836de1d1d70d807c42
gkstjr <gkstjr8332@naver.com>
2023-03-05 15:52:09 +0900

    상품 주문 객체, 로직, 테스트 구현

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,6 +21,11 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        return em.createQuery("select o from Order o join o.member m",
+//                .setMaxResults(1000) //최대 1000건
+//                .getResultList();
+//    }
 
 }
