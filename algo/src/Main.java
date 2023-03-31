1a48dcdc6639365aaf9b7b29e5c6aa7113275bed
gkstjr <gkstjr8332@naver.com>
2023-03-31 16:56:25 +0900

    브루트 포스1

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -1,43 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
 public class Main {
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st;
 		
-		int[][] box = new int[100][100];
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int arr[] = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i =0 ; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		
-		int n = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			for(int j = y ; j<y+10; j++) {
-				for(int k = x; k < x + 10; k++) {
-					box[j][k] = 1;
+		int result = search(arr, n, m);
+		System.out.println(result);
+	
+	
+	}
+
+	private static int search(int[] arr, int n, int m) {
+		int result = 0;
+		for(int i =0 ; i< n-2; i++) {
+			for(int j = i + 1; j < n-1; j++) {
+				for(int k = j + 1; k < n; k++) {
+					
+					int temp = arr[i] + arr[j] + arr[k];
+					if (temp == m ) return temp;
+					
+					if(temp > result && temp < m) {
+						result = temp;
+					}
 				}
 			}
 		}
-		int sum = 0;
-		for(int i = 0; i< box.length; i++) {
-			for(int j = 0; j<box[i].length; j++) {
-				sum += box[i][j];
-			}
-		}
-		System.out.println(sum);
+		
+		return result;
 	}
 }	
