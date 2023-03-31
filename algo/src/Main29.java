f019e69c72e4cb4ef251318c51bd6f96dd643b61
gkstjr <gkstjr8332@naver.com>
2023-03-31 16:57:39 +0900

    백준 시간복잡도 문제 / 브루트포스 문제

--- algo/src/Main29.java
+++ algo/src/Main29.java
@@ -1,21 +1,7 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 
 public class Main29 {
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		//단일 연산이므로 연산을 하는 횟수만큼 시간 복잡도 고
-	
-		
+	public void main(String[] args) {
 		System.out.println("1");
 		System.out.println("0");
 	}	
