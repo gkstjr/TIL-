43262bea5614d5acf27ff0fd85f9d764810715fb
gkstjr <gkstjr8332@naver.com>
2023-04-04 10:08:17 +0900

    선택정렬 알고리즘 풀이

---
+++ algo/src/Main37.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main37 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i =0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		//오름차순으로 정렬
+		for(int i =0; i < arr.length - 1; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i = 0 ; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}	
+}
