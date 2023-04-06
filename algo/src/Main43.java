a931d62361ac3d8143db59020fd3e6d9f247f290
gkstjr <gkstjr8332@naver.com>
2023-04-06 22:31:56 +0900

    정렬 알고리즘 시간초과 안되고 빠르게 할 때 (Collections.sort) 이용

---
+++ algo/src/Main43.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main43 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		for(int i = 0 ; i< N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		
+		System.out.println(sb);
+	}	
+}
