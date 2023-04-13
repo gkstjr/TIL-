9dd88868274c7899fc97be287c210db106c08dae
gkstjr <gkstjr8332@naver.com>
2023-04-13 23:43:56 +0900

    알고리즘 개념 복습함(자료구조쪽)

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -1,45 +1,48 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+ 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st;
 		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-
-		int arr[] = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for(int i =0 ; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int result = search(arr, n, m);
-		System.out.println(result);
-	
-	
-	}
-
-	private static int search(int[] arr, int n, int m) {
-		int result = 0;
-		for(int i =0 ; i< n-2; i++) {
-			for(int j = i + 1; j < n-1; j++) {
-				for(int k = j + 1; k < n; k++) {
-					
-					int temp = arr[i] + arr[j] + arr[k];
-					if (temp == m ) return temp;
-					
-					if(temp > result && temp < m) {
-						result = temp;
-					}
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i = 0 ; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String order = st.nextToken();
+			if(order.equals("push")) {
+				int num  = Integer.parseInt(st.nextToken());
+				stack.push(num);
+			}else if(order.equals("pop")) {
+				if(stack.size() == 0 ) {
+					System.out.println(-1);
+				}else {					
+					System.out.println(stack.pop());
+				}					
+			}else if(order.equals("size")) {
+				System.out.println(stack.size());
+			}else if(order.equals("empty")) {
+				if(stack.size() == 0) {
+					System.out.println(1);
+				}else System.out.println(0);
+			}else if(order.equals("top")) {
+				if(stack.size() == 0) {
+					System.out.println(-1);
+				}else {		
+				System.out.println(stack.peek());
 				}
 			}
+			
 		}
-		
-		return result;
-	}
-}	
+	}	
+}
