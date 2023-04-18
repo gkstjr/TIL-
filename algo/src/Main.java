e66aafefd122424394e480fb3bb3c47d78bde7c8
gkstjr <gkstjr8332@naver.com>
2023-04-18 18:20:20 +0900

    깃허브 연동테스트

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -11,38 +11,40 @@ import java.util.HashMap;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
  
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st;
+		for(int i = 0; i < n; i++) {
+			sb.append(solve(br.readLine())).append("\n");
+		}
+		System.out.println(sb);
+	}	
 		
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<Integer>();
-		for(int i = 0 ; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String order = st.nextToken();
-			if(order.equals("push")) {
-				int num  = Integer.parseInt(st.nextToken());
-				stack.push(num);
-			}else if(order.equals("pop")) {
-				if(stack.size() == 0 ) {
-					System.out.println(-1);
-				}else {					
-					System.out.println(stack.pop());
-				}					
-			}else if(order.equals("size")) {
-				System.out.println(stack.size());
-			}else if(order.equals("empty")) {
-				if(stack.size() == 0) {
-					System.out.println(1);
-				}else System.out.println(0);
-			}else if(order.equals("top")) {
-				if(stack.size() == 0) {
-					System.out.println(-1);
-				}else {		
-				System.out.println(stack.peek());
+		public static String solve(String s) {
+			String answer = "";
+			Stack<Character> sk = new Stack<Character>();
+			for(int i = 0; i < s.toCharArray().length; i++) {
+				char ch = s.charAt(i);
+				
+				if(ch == '(') {
+					sk.push(ch);
+				}else if(ch == ')') {
+					if(sk.isEmpty()) {
+						return "NO";
+					}else {
+						sk.pop();						
+					}
+					
 				}
 			}
 			
+			if(sk.isEmpty()) {
+				answer = "YES";
+			}else {
+				answer = "NO";
+			}
+			
+			return answer;
 		}
-	}	
 }
