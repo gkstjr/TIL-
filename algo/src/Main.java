f0dc96fa9520d8d60018399fd358ae71e4f63abd
gkstjr <gkstjr8332@naver.com>
2023-04-25 22:48:33 +0900

    큐 알고리즘2

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -7,44 +7,54 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
  
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
  
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		Queue<Integer> que = new LinkedList<Integer>();
+		int last = 0;
 		int n = Integer.parseInt(br.readLine());
-		
 		for(int i = 0; i < n; i++) {
-			sb.append(solve(br.readLine())).append("\n");
-		}
-		System.out.println(sb);
-	}	
-		
-		public static String solve(String s) {
-			String answer = "";
-			Stack<Character> sk = new Stack<Character>();
-			for(int i = 0; i < s.toCharArray().length; i++) {
-				char ch = s.charAt(i);
-				
-				if(ch == '(') {
-					sk.push(ch);
-				}else if(ch == ')') {
-					if(sk.isEmpty()) {
-						return "NO";
-					}else {
-						sk.pop();						
-					}
-					
+			st = new StringTokenizer(br.readLine());
+			String order = st.nextToken();
+			if(order.equals("push")) {
+				int num = Integer.parseInt(st.nextToken());
+				que.offer(num);
+				last = num;
+			}else if(order.equals("pop")) {
+				if(que.isEmpty()) {
+					sb.append("-1").append("\n");
+				}else {
+					int num = que.poll();
+					sb.append(num).append("\n");
 				}
+			}else if(order.equals("size")) {
+				sb.append(que.size()).append("\n");
+			}else if(order.equals("empty")) {
+				if(que.isEmpty()) {
+					sb.append("1").append("\n");
+				}else {
+					sb.append("0").append("\n");
+				}
+			}else if(order.equals("front")) {
+				if(que.isEmpty()) {
+					sb.append("-1").append("\n");
+				}else {
+					sb.append(que.peek()).append("\n");
+				}			
+			}else if(order.equals("back")) {
+				if(que.isEmpty()) {
+					sb.append("-1").append("\n");
+				}else {
+					sb.append(last).append("\n");
+				}	
 			}
-			
-			if(sk.isEmpty()) {
-				answer = "YES";
-			}else {
-				answer = "NO";
-			}
-			
-			return answer;
-		}
+	}
+		System.out.println(sb);
+}
 }
