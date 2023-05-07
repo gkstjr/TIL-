66a682a98abc2a4f566a73caff80e2a985c4ff74
gkstjr <gkstjr8332@naver.com>
2023-05-07 22:09:29 +0900

    이분탐색 알고리즘

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -1,50 +1,70 @@
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.StringTokenizer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.StringTokenizer;
  
 public class Main {
-	 
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		//최소값을 우선순위로 정렬하기
-		PriorityQueue<Integer> minQueue = new PriorityQueue<Integer>();
-		//최대값을 우선순위로 정렬
-		PriorityQueue<Integer> maxQueue = new PriorityQueue<Integer>(Collections.reverseOrder());
-
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
 		
-		for(int i = 0; i < n; i++) {
-			int temp = Integer.parseInt(br.readLine());
-			//최소값 큐와 최대값 큐의 사이즈가 같을 때 최대값에 넣기
-			if(minQueue.size() == maxQueue.size()) maxQueue.offer(temp);
-			else minQueue.offer(temp);
-			// 바꿔주기
-			if(!minQueue.isEmpty() && minQueue.peek() < maxQueue.peek()) {
-				int swap1 = minQueue.poll();
-				int swap2 = maxQueue.poll();
-				maxQueue.offer(swap1);
-				maxQueue.offer(swap2);
-			}
-			sb.append(maxQueue.peek()).append("\n");
+		for(int i = 0 ; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(arr);
 		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0 ; i < M; i++) {
+			int key = Integer.parseInt(st.nextToken());
+			
+			sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(" ");
+		}
 		System.out.println(sb);
+	}
+	// 하한 인덱스 구하기
+	public static int lowerBound(int[] arr , int key) {
+		
+		int lo = 0;
+		int hi = arr.length;
+		
+		while(lo < hi) {
+			int mid = (lo + hi) / 2;
+			
+			if(key <= arr[mid]) {
+				hi = mid;
+			}else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
 	
-	}	
+	//상한 인덱스 쿠하기
+	public static int upperBound(int[] arr, int key) {
+		
+		int lo = 0;
+		int hi = arr.length;
+		
+		while(lo < hi) {
+			int mid = (lo + hi) / 2;
+			
+			if(key < arr[mid]) {
+				hi = mid;
+			}else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
 }
