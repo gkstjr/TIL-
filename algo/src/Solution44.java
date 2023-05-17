bfa082982f2857e6a7cbff50d19ab557471365f5
gkstjr <gkstjr8332@naver.com>
2023-05-17 15:29:51 +0900

    프로그래머스 문자열 문제 풀이

--- algo/src/Solution44.java
+++ algo/src/Solution44.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
 import java.util.*;
 
-class Solution {	
+class Solution44 {	
 		 public static void main(String[] args) {
-	    	Solution sol = new Solution();
+	    	Solution44 sol = new Solution44();
 //	    	int[] num = {0,1,2,4,3};
 	    	int[][] num2 = {{2,3},{0,7},{5,9},{6,10}};
 //	    	System.out.println(num[1]);
 //	    	System.out.println(sol.solution("rermgorpsam", num2));
 	    } 
-		 public int solution(String my_string, String is_suffix) {
+		 public String[] solution(String my_string) {
 		        String[] answer = new String[my_string.length()];
-		        int cnt = 0;
 		        for(int i = 0 ; i < my_string.length(); i++) {
 		        	answer[i] = my_string.substring(i,my_string.length());
-		        	if(answer[i].equals(is_suffix)) {
-		        		cnt = 1;
-		        	}
 		        }
-		        return cnt;
+		        Arrays.sort(answer);
+		        return answer;
 		    }
 }
--- algo/src/Solution45.java
+++ algo/src/Solution45.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.*;
 
-class Solution {	
+class Solution45 {	
 		 public static void main(String[] args) {
-	    	Solution sol = new Solution();
+	    	Solution45 sol = new Solution45();
 //	    	int[] num = {0,1,2,4,3};
 	    	int[][] num2 = {{2,3},{0,7},{5,9},{6,10}};
 //	    	System.out.println(num[1]);
