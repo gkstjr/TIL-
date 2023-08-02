a108e8629193cb01d670a1d025b82ff17de434ef
gkstjr <gkstjr8332@naver.com>
2023-08-03 08:42:54 +0900

    Day01

---
+++ src/august1/Day01_2558.java
@@ -0,0 +1,17 @@
+package august1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day01_2558 {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a + b);
+	}
+}
