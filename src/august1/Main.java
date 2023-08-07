f61665c5484e81201fa14823859d4290672f1ea8
gkstjr <gkstjr8332@naver.com>
2023-08-07 14:39:40 +0900

    Day07_11727 문제풀이

--- src/august1/Main.java
+++ src/august1/Main.java
@@ -8,45 +8,29 @@ import java.util.Calendar;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-/*
- 	x를 	1) 3, 2의 최소공배수 6알 때 
- 	    2) 3으로 나누어 질 때
- 	    3) 2로만 나누어 질 때
- 	    4) 아무것도 나누어 지지 않을 때
- 	    
- 	    특이사항) 3 이나 2로 나누어 진다고 해서 1 뺴는 것 보다 최소값이 나오는 건 아니다.
- * */
-
+		/* 
+		 *  n - 1 의 1씩 추가되고 
+		 *  n - 2 의 추가되고
+		 * 	dp[n]  = 해당 2 * n 을 채우는 방법의 수
+		 * */
 public class Main {
-		static Integer[] dp;
-		
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+			
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		dp = new Integer[n + 1];
-		dp[0] = dp[1] = 0;
+		Integer[] dp = new Integer[n + 1];
 		
-		System.out.println(recur(n));
+		dp[0] = 1;
+		dp[1] = 1;
 		
-	}
-	static int recur(int n) {
 		
-		if(dp[n] == null) {
-			if(n % 6 == 0) {
-				dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-			}else if(n % 3 == 0) {
-				dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-			}else if(n % 2 == 0) {
-				dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-			}else {
-				dp[n] = recur(n - 1) + 1;
-			}
+		for(int i = 2 ; i < n + 1; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 		}
-		
-		return dp[n];
-		
+ 		
+		System.out.println(dp[n]);
+	
 	}
 }
