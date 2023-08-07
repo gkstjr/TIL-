5a2ba9bcea45166669e719d1b6a3b8ac186678f4
gkstjr <gkstjr8332@naver.com>
2023-08-07 10:51:20 +0900

    Day07(DP 문제 풀이) 주말 한개도 안하다니... 하루에 3개씩은 꼭 해보자

---
+++ src/august1/Day07_11726.java
@@ -0,0 +1,36 @@
+package august1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+		/* 
+		 *  n - 1 의 1씩 추가되고 
+		 *  n - 2 의 추가되고
+		 * 	dp[n]  = 해당 2 * n 을 채우는 방법의 수
+		 * */
+public class Day07_11726 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Integer[] dp = new Integer[n + 1];
+		
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		
+		for(int i = 2 ; i < n + 1; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+ 		
+		System.out.println(dp[n]);
+	
+	}
+}
