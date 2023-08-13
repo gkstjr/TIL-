344eefd9e4246c16110f0c1a9ebeba4d31a96a7e
gkstjr <gkstjr8332@naver.com>
2023-08-14 01:10:15 +0900

    Dp 뒤의 문제 자력 쉽지 않다

---
+++ src/august1/Day13_11052.java
@@ -0,0 +1,33 @@
+package august1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+ * 	dp[n]은 n개의 카드를 가질 때 가장 큰 최댓
+ * */
+class Day13_11052 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] dp = new int[n + 1];
+        int[] seq = new int[n + 1];
+        
+        for(int i = 1; i < n + 1; i++) {
+        	seq[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp[1] = seq[1];
+        
+        for(int i = 2; i < n + 1; i ++) {
+        	for(int j = i; j >= 1; j--) {
+        		dp[i] = Math.max(dp[i], seq[j] + dp[i - j]);
+        	}
+        }
+        
+        System.out.println(dp[n]);
+       }
+}
