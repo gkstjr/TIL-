27f82072b6da592ccbf48216d3967dabc42f39cf
gkstjr <gkstjr8332@naver.com>
2023-08-17 18:02:58 +0900

    11004 푸는 도중임

--- src/august1/Main.java
+++ src/august1/Main.java
@@ -2,6 +2,8 @@ package august1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 /*
  * 	dp[n]은 n개의 카드를 가질 때 가장 큰 최댓
@@ -10,24 +12,29 @@ class Main {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
         
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] dp = new int[n + 1];
-        int[] seq = new int[n + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         
-        for(int i = 1; i < n + 1; i++) {
-        	seq[i] = Integer.parseInt(st.nextToken());
+        boolean[] arr = new boolean[200000001];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1 ; i < n + 1; i++) {
+        	int num = Integer.parseInt(st.nextToken());
+        	arr[num] = true;
         }
         
-        dp[1] = seq[1];
-        
-        for(int i = 2; i < n + 1; i ++) {
-        	for(int j = i; j >= 1; j--) {
-        		dp[i] = Math.max(dp[i], seq[j] + dp[i - j]);
+        int cnt = 0;
+        for(int i = 1 ; i < 200000001; i++ ) {
+        	if(arr[i] == true) {
+        		cnt++;
+        		if(cnt == k) {
+        			System.out.println(i);
+        			break;
+        		}
         	}
         }
-        
-        System.out.println(dp[n]);
        }
 }
