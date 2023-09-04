27da1180028b35a3294233bfd7157556db7d273f
gkstjr <gkstjr8332@naver.com>
2023-09-04 09:01:38 +0900

    이사 핑계로 3일동안 코테 공부 안함.... 다시 달리자

---
+++ src/august1/Day31_11576.java
@@ -0,0 +1,65 @@
+package august1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.prefs.BackingStoreException;
+/*
+
+ *  'A' = 65
+ * */
+class Day31_11576 {
+    public static void main(String args[]) throws Exception {
+     
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st;
+    	// 진법 정보
+    	st = new StringTokenizer(br.readLine());
+    	
+    	int A = Integer.parseInt(st.nextToken());
+    	int B = Integer.parseInt(st.nextToken());
+    	
+    	//A진법으로 나타낸 수의 자리수
+    	int m = Integer.parseInt(br.readLine());
+    	
+    	//A진법으로 자릿수
+    	
+    	st = new StringTokenizer(br.readLine());
+    	long[] arr = new long[m];
+    	
+    	int square = 1;
+    	long sum = 0;
+    	for(int i = 0 ; i < m; i++) {
+    		arr[i] = Long.parseLong(st.nextToken());  		
+    	}
+    	for(int i = arr.length - 1; i >= 0; i--) {
+    		sum += arr[i] * square;
+    		square *= A;
+    	}
+    	
+    	Stack<Long> stk = new Stack<Long>();
+    	
+    	while(sum != 0) {
+    		stk.push(sum % B);
+    		
+    		sum /= B;
+    	}
+    
+    	
+    	while(!stk.isEmpty()) {
+    		if(stk.size() == 1) {
+    			System.out.print(stk.peek());
+    		}else {
+    			System.out.print(stk.peek() + " ");
+    		}
+    		
+    		stk.pop();
+    	}
+    }
+}
