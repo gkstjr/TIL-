236f46ea202cda1e6b7d912f7aed9beb2b0a1f05
gkstjr <gkstjr8332@naver.com>
2023-09-05 08:25:06 +0900

    소인수 분해, 팩토리얼 문제 풀이

--- src/september/Day05_6588.java
+++ src/september/Day05_6588.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Day04_6588 {
+public class Day05_6588 {
 	static int n;
 	static boolean[] arr = new boolean[1000001]; 
 	public static void main(String[] args) throws IOException {
