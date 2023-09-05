3ce7836040ca70c82049482c446a173041fb1655
gkstjr <gkstjr8332@naver.com>
2023-09-06 08:41:23 +0900

    깃허브 정리

--- src/september/Day05_11653.java
+++ src/september/Day05_11653.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Day04_11653 {
+public class Day05_11653 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
