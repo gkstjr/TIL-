cf6dd859ddcd6ec82858cfe2e25d0d3f3369fb3a
gkstjr <gkstjr8332@naver.com>
2023-09-26 17:35:22 +0900

    후위 표기식 푸는중

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -5,44 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+//피연산자는 결과에 넣고 연산자는 스택에 넣기
+//연산자를 스택에 넣을 때 스택에 가장 최상단이 넣을 연산자보다 우선순위가 낮을때까지 결과에 pop하기
+//"("는 스택에 넣고 ")" 만나면 스택에서 (가 나올기 전에 연산자들을 pop해서 결과에 넣기   
 public class Main {
-	static Stack<Character> stk = new Stack<Character>();
-	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		Stack<Character> stk = new Stack<Character>();
+		
 		String str = br.readLine();
-		
-		boolean tag = false;
-		
-		for(char ch : str.toCharArray()) {
-			
-			if(ch == '<') {
-				print();
-				tag = true;
-				sb.append(ch);
-			}else if(ch == '>') {
-				tag=false;
-				sb.append(ch);
-			}else if(tag) {
-				sb.append(ch);
-			}else if(ch == ' ') {
-				print();
-				sb.append(ch);
+		String ans = "";
+		for(int i = 0 ; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'A' && ch <= 'Z') {
+				ans += ch;
 			}else {
-				stk.push(ch);
+				if(ch == ')') {
+					while(stk.peek() != '(') {
+						ans += stk.pop();
+					}
+					stk.pop();
+				}else if(ch == '('){
+					stk.push(ch);
+				}else {
+					while(!stk.isEmpty() && precedence(stk.peek()) > precedence(ch)) {
+						ans += stk.pop();
+					}
+					stk.push(ch);
+				}
 			}
 		}
-		
-		print();
-		
-		
-		System.out.println(sb);
+	
+	while(!stk.isEmpty()) {
+		ans += stk.pop();
 	}
-	static void print() {
-		while(!stk.isEmpty()) {
-			sb.append(stk.pop());
-		}
+	
+	System.out.println(ans);
+	}
+	
+	static int precedence(char ch) {
+		if(ch == '+' || ch == '-') return 1;
+		if(ch == '*' || ch == '/') return 2;
+		return 0;
 	}
 }
