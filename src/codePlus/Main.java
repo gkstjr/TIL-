3f95f66b12f252bee9e080efb6c5087e0d3d1279
gkstjr <gkstjr8332@naver.com>
2023-10-13 01:11:43 +0900

    알고리즘 정리

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -5,22 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-// 알고리즘 보류
+/* 
+	dp[n] = 정수 n 을 1, 2 , 3 의 합으로 나타내는 방법의 수 
+	점화식 -> dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3];
+*/
 public class Main {
+	static Integer[] dp;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 		
-		int num = Integer.parseInt(br.readLine());
-		
-		int count = 0 ;
-		
-		
-		while(num >= 5) {
-			count += num / 5;
-			num /= 5;
+		for(int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			dp = new Integer[12];
+			dp[1] = 1; dp[2] = 2; dp[3] = 4;
 			
+			sb.append(recur(n)).append("\n");
 		}
-		System.out.println(count);
+		System.out.println(sb);
 	}
+	
+	static int recur(int n) {
+		if(dp[n] == null) {
+			dp[n] = recur(n - 1) + recur(n - 2) + recur(n - 3);
+		}
+		
+		return dp[n];
+	}
+
 }
