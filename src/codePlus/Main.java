9c2932c3b21018b9149cb9a1623936da70f75034
gkstjr <gkstjr8332@naver.com>
2023-10-18 23:03:23 +0900

    깃허브 정리

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -4,39 +4,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
-/* 
-	dp[n] = 정수 n 을 1, 2 , 3 의 합으로 나타내는 방법의 수 
-	점화식 -> dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3];
-	
+/* 	
 	
 */
-public class Main {
-	static Integer[] dp;
+public class Main {	
+	static int[] dp;
+	static int[] A;
+	static int[] lastIndex;
+	static StringBuilder sb;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < T; i++) {
-			int n = Integer.parseInt(br.readLine());
-			dp = new Integer[12];
-			dp[1] = 1; dp[2] = 2; dp[3] = 4;
-			
-			sb.append(recur(n)).append("\n");
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		dp = new int[n + 1];
+		A = new int[n + 1];
+		lastIndex = new int[n + 1];
+		
+		for(int i = 1; i <= n; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		for(int i = 1; i <= n; i++) {
+			dp[i] = 1;
+			for(int j = 1; j <= i - 1; j++) {
+				if(dp[j] + 1 > dp[i] && A[j] < A[i]) {
+					dp[i] = dp[j] + 1;
+					lastIndex[i] = j;
+				}
+			}
+		}
+		
+		//dp의 값중에 가장 긴 수열인 값 찾기 because 현재 dp[i] 는 i에서의 수열의 길이를 보여주기때문
+		int answer = 0;
+		int answerIndex = 0;
+		for(int i = 1; i <= n; i++) {
+			if(dp[i] > answer) {
+				answer = dp[i];
+				answerIndex = i;
+			}
+		}
+		sb = new StringBuilder();
+		
+		sb.append(answer).append("\n");
+		recur(answerIndex);
+		
 		System.out.println(sb);
-	}
-	
-	static int recur(int n) {
-		if(dp[n] == null) {
-			dp[n] = recur(n - 1) + recur(n - 2) + recur(n - 3);
-		}
-		
-		return dp[n];
+ 	}
+	static void recur(int n) {
+		if(n == 0) return;
+		recur(lastIndex[n]);
+		sb.append(A[n]).append(" ");
 		
 	}
-
 }
