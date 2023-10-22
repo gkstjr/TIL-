84c05468fe8c29fa8ec440187c79fd40b6da7d7d
gkstjr <gkstjr8332@naver.com>
2023-10-22 21:06:15 +0900

    알고리즘 주말 정리

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -7,60 +7,32 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 /* 	
-	
+ 	dp[k][n] = dp[k - 1][n - i] 의 i의 범위 안에서의 모든 합을 더해야 함
+	초기셋팅 (범위)
+	0 <= i <= n;
 */
-public class Main {	
-	static int[] dp;
-	static int[] A;
-	static int[] lastIndex;
-	static StringBuilder sb;
+ public class Main {	
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		dp = new int[n + 1];
-		A = new int[n + 1];
-		lastIndex = new int[n + 1];
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 		
-		for(int i = 1; i <= n; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
+		long[][] dp = new long[k + 1][n + 1];
+		dp[0][0] = 1;
 		
-		for(int i = 1; i <= n; i++) {
-			dp[i] = 1;
-			for(int j = 1; j <= i - 1; j++) {
-				if(dp[j] + 1 > dp[i] && A[j] < A[i]) {
-					dp[i] = dp[j] + 1;
-					lastIndex[i] = j;
+		for(int i = 1; i <= k; i++) {
+			for(int j = 1; j <= n; j++) {
+				for(int l = 0; l <= j; l++ ) {
+					dp[i][j] += dp[i - 1][j - l];
 				}
+				dp[i][j] %= 1000000000; 
 			}
 		}
 		
-		//dp의 값중에 가장 긴 수열인 값 찾기 because 현재 dp[i] 는 i에서의 수열의 길이를 보여주기때문
-		int answer = 0;
-		int answerIndex = 0;
-		for(int i = 1; i <= n; i++) {
-			if(dp[i] > answer) {
-				answer = dp[i];
-				answerIndex = i;
-			}
-		}
-		sb = new StringBuilder();
-		
-		sb.append(answer).append("\n");
-		recur(answerIndex);
-		
-		System.out.println(sb);
- 	}
-	static void recur(int n) {
-		if(n == 0) return;
-		recur(lastIndex[n]);
-		sb.append(A[n]).append(" ");
-		
+		System.out.println(dp[k][n] % 1000000000);
 	}
+
 }
