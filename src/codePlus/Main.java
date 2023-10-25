09ac47edc0a32b6b35a36bd418eeae7ccf323be5
gkstjr <gkstjr8332@naver.com>
2023-10-26 01:01:24 +0900

    restAPI 강의 refresh으로 듣기

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -2,37 +2,61 @@ package codePlus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-/* 	
- 	dp[k][n] = dp[k - 1][n - i] 의 i의 범위 안에서의 모든 합을 더해야 함
-	초기셋팅 (범위)
-	0 <= i <= n;
+/* 		
+ * 	restApi 강의 일주일 듣기
+	
+	세가지 값중에 최댓값을 구할 것
 */
  public class Main {	
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[n + 2];
+		int[] dp2 = new int[n + 2];
+		int[] seq = new int[n + 1];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+		for(int i = 1; i <= n; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
 		
-		long[][] dp = new long[k + 1][n + 1];
-		dp[0][0] = 1;
-		
-		for(int i = 1; i <= k; i++) {
-			for(int j = 1; j <= n; j++) {
-				for(int l = 0; l <= j; l++ ) {
-					dp[i][j] += dp[i - 1][j - l];
-				}
-				dp[i][j] %= 1000000000; 
+		//i를 끝으로 하는 연속
+		for(int i = 1; i <= n; i++) {
+			dp[i] = seq[i];
+			if(dp[i - 1] + seq[i] > dp[i]) {
+				dp[i] = dp[i - 1] + seq[i];
 			}
 		}
 		
-		System.out.println(dp[k][n] % 1000000000);
+		//i를 시작으로 하는 연속합
+		for(int i = n; i >= 1; i--) {
+			if(i == n) {
+				dp2[i] = seq[i];
+				continue;
+			}
+			dp2[i] = Math.max(dp2[i + 1] + seq[i], seq[i]);
+		}
+	
+		// 제외 안하고 최댓값과 제외 하고 최댓값중에 더 큰 값 찾기
+		int answer = dp[1];
+		
+		for(int i = 1; i <=n; i++) {
+			answer = Math.max(dp[i], answer);
+		}
+		
+		for(int i = 2; i <= n; i++) {
+			answer = Math.max(dp[i - 1] + dp2[i + 1],answer);
+		}
+		
+		System.out.println(answer);
 	}
 
 }
