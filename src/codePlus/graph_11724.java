9e205558d7ad215d4e678d4b649c166e3117748a
gkstjr <gkstjr8332@naver.com>
2023-11-25 22:19:08 +0900

    그래프 문제 풀이

--- src/codePlus/graph_11724.java
+++ src/codePlus/graph_11724.java
@@ -4,12 +4,10 @@ import java.util.*;
 import java.io.*;
 
 
-public class Main { 
-	
+public class graph_11724 { 
 	static ArrayList<Integer>[] list;
 	static boolean[] check;
-	static StringBuilder sb = new StringBuilder();
-	
+	static int answer;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -18,40 +16,37 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int V = Integer.parseInt(st.nextToken());
 		
 		list = new ArrayList[N + 1];
 		check = new boolean[N + 1];
 		
-		for(int i = 1; i < N + 1; i++) {
+		for(int i = 1 ; i < N + 1; i++) {
 			list[i] = new ArrayList<Integer>();
 		}
 		
-		for(int i = 0 ; i < M; i++) {
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
 			list[a].add(b);
 			list[b].add(a);
-			
-		}
-		for(int i = 1 ; i < N + 1; i++) {
-			Collections.sort(list[i]);
 		}
 		
-		dfs(V);
-		sb.append("\n");
-		Arrays.fill(check, false);
-		bfs(V);
-		System.out.println(sb);
+		for(int i = 1; i < N + 1; i++) {
+			if(!check[i]) {
+				answer++;
+				dfs(i);
+			}
+		}
 		
+		System.out.println(answer);
 	}
 	
 	static void dfs(int v) {
 		check[v] = true;
-		sb.append(v).append(" ");
-		ㅇㄹㅇㄹ
+		
 		for(int i : list[v]) {
 			if(!check[i]) {
 				dfs(i);
@@ -59,22 +54,4 @@ public class Main {
 		}
 		return;
 	}
-	
-	static void bfs(int v) {
-		check[v] = true;
-		Queue<Integer> que = new LinkedList<Integer>();
-		que.offer(v);
-		sb.append(v).append(" ");
-		while(!que.isEmpty()) {
-			for(int i  : list[que.poll()]) {
-				if(!check[i]) {
-					que.offer(i);
-					sb.append(i).append(" ");
-					check[i] = true;
-				}
-			}
-		}
-		return;
-	}
-	
 }
