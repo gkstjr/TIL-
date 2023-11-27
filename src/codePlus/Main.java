d5e057ad6546eaaadaece20e546657653ed9476e
gkstjr <gkstjr8332@naver.com>
2023-11-27 21:03:30 +0900

    네트워크 4주차 정리

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -2,79 +2,73 @@ package codePlus;
 
 import java.util.*;
 import java.io.*;
+import java.nio.Buffer;
 
 
 public class Main { 
-	
 	static ArrayList<Integer>[] list;
-	static boolean[] check;
-	static StringBuilder sb = new StringBuilder();
+	static int[] check; 
+	static int V;
+	static int E;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int V = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[N + 1];
-		check = new boolean[N + 1];
-		
-		for(int i = 1; i < N + 1; i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-		
-		for(int i = 0 ; i < M; i++) {
+		for(int t = 0 ; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
 			
-			list[a].add(b);
-			list[b].add(a);
+			list = new ArrayList[V + 1];
+			check = new int[V + 1];
 			
-		}
-		for(int i = 1 ; i < N + 1; i++) {
-			Collections.sort(list[i]);
-		}
-		
-		dfs(V);
-		sb.append("\n");
-		Arrays.fill(check, false);
-		bfs(V);
-		System.out.println(sb);
-		
-	}
-	
-	static void dfs(int v) {
-		check[v] = true;
-		sb.append(v).append(" ");
-		ㅇㄹㅇㄹ
-		for(int i : list[v]) {
-			if(!check[i]) {
-				dfs(i);
+			for(int i = 1; i <= V; i++) {
+				list[i] = new ArrayList<Integer>();
 			}
-		}
-		return;
-	}
-	
-	static void bfs(int v) {
-		check[v] = true;
-		Queue<Integer> que = new LinkedList<Integer>();
-		que.offer(v);
-		sb.append(v).append(" ");
-		while(!que.isEmpty()) {
-			for(int i  : list[que.poll()]) {
-				if(!check[i]) {
-					que.offer(i);
-					sb.append(i).append(" ");
-					check[i] = true;
+			for(int i = 0; i < E; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				list[a].add(b);
+				list[b].add(a);
+			}
+			for(int i =1; i <= V; i++) {
+				if(check[i] == 0) dfs(i , 1);
+			}
+			
+			// 정점과 간선의 그룹 값이 같은 지 다른 지 찾기
+			boolean ok = true;
+			Loop1 : 
+			for(int i = 1; i <= V; i++ ) {
+				for(int j = 0; j < list[i].size(); j++) {
+					int num = list[i].get(j);
+					if(check[i] == check[num]) {
+						ok = false;
+						break Loop1;
+					}
 				}
 			}
+			if(ok) sb.append("YES");
+			else sb.append("NO");
+			sb.append("\n");
 		}
-		return;
+		
+		System.out.println(sb);
+	}
+	static void dfs(int node ,int c) {
+		check[node] = c;
+		
+		
+		for(int i : list[node]) {
+			if(check[i] == 0) {
+				dfs(i , 3 - c);
+			}
+		}
 	}
 	
 }
