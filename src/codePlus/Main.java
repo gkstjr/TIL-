152a5678297392aaf74bcc7647bb4a3769307b6b
gkstjr <gkstjr8332@naver.com>
2023-12-02 23:12:47 +0900

    네트워크스터디 5주차 마지막

--- src/codePlus/Main.java
+++ src/codePlus/Main.java
@@ -6,69 +6,63 @@ import java.nio.Buffer;
 
 
 public class Main { 
-	static ArrayList<Integer>[] list;
-	static int[] check; 
-	static int V;
-	static int E;
+	static String[][] list;
+	static int[] dx = {0,0,1,-1};
+	static int[] dy = {1,-1,0,0};
+	static boolean[][] check;
+	static int FirstJ; //열
+	static int FirstI; //행
+	static int N; // 행
+	static int M; // 열
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int T = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		for(int t = 0 ; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			
-			list = new ArrayList[V + 1];
-			check = new int[V + 1];
-			
-			for(int i = 1; i <= V; i++) {
-				list[i] = new ArrayList<Integer>();
+		list = new String[N][M];
+		
+		for(int i = 0 ; i < N; i++) {
+			String str = br.readLine();
+			for(int j = 0; j < M; j++) {
+				list[i][j] = String.valueOf(str.charAt(j));
 			}
-			for(int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				list[a].add(b);
-				list[b].add(a);
-			}
-			for(int i =1; i <= V; i++) {
-				if(check[i] == 0) dfs(i , 1);
-			}
-			
-			// 정점과 간선의 그룹 값이 같은 지 다른 지 찾기
-			boolean ok = true;
-			Loop1 : 
-			for(int i = 1; i <= V; i++ ) {
-				for(int j = 0; j < list[i].size(); j++) {
-					int num = list[i].get(j);
-					if(check[i] == check[num]) {
-						ok = false;
-						break Loop1;
-					}
+		}
+		
+		for(int i = 0 ; i < N; i++) {
+			for(int j= 0; j < M; j++) {
+				check = new boolean[N][M];
+				FirstJ = j; //열
+				FirstI = i; // 행
+				if(dfs(i , j , 0)) { // true면 YES
+					System.out.println("Yes");
+					return;
 				}
 			}
-			if(ok) sb.append("YES");
-			else sb.append("NO");
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-	static void dfs(int node ,int c) {
-		check[node] = c;
-		
-		
-		for(int i : list[node]) {
-			if(check[i] == 0) {
-				dfs(i , 3 - c);
-			}
-		}
+ 		}
+		System.out.println("No");
 	}
 	
-}
+	static boolean dfs(int i , int j , int cnt ) {
+		if(check[i][j]) {
+			if(cnt >= 4 && i == FirstI && j == FirstJ) return true;
+			else return false;
+		}
+		check[i][j] = true;
+		
+		for(int k = 0; k < 4; k++) {
+			int nextI = i + dy[k];
+			int nextJ = j + dx[k];
+			
+			if(nextI >= 0 && nextI < N && nextJ >= 0 && nextJ < M) {
+				if(list[i][j].equals(list[nextI][nextJ])) {
+					if(dfs(nextI, nextJ , cnt + 1)) return true;
+				}
+			}
+		}
+		
+		return false;		
+	}
+ }
