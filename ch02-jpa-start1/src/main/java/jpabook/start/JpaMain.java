4aa47f062206fbd022b0fb92c34d76a9feb056f0
gkstjr <gkstjr8332@naver.com>
2023-12-23 01:11:30 +0900

    h2 버전 업데이트 수

--- ch02-jpa-start1/src/main/java/jpabook/start/JpaMain.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/JpaMain.java
@@ -35,28 +35,31 @@ public class JpaMain {
 
     public static void logic(EntityManager em) {
 
-        String id = "id1";
-        MemberBack member = new MemberBack();
-        member.setId(id);
-        member.setUsername("지한");
-        member.setAge(2);
-
-        //등록
-        em.persist(member);
-
-        //수정
-        member.setAge(20);
-
-        //한 건 조회
-        MemberBack findMember = em.find(MemberBack.class, id);
-        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
-
-        //목록 조회
-        List<MemberBack> members = em.createQuery("select m from Member m", MemberBack.class).getResultList();
-        System.out.println("members.size=" + members.size());
-
-        //삭제
-        em.remove(member);
-
+    	String id = "id1";
+    	Member member = new Member();
+    	member.setId(id);
+    	member.setUsername("한석");
+    	member.setAge(28);
+    	
+    	//등록
+    	em.persist(member);
+    	
+    	//수정
+    	member.setAge(29);
+    	
+    	//한건 조회
+    	Member findMember = em.find(Member.class, id);
+    	System.out.println("findMember= " + findMember.getUsername() + ", age = " + findMember.getAge());
+    	
+    	//목록 조회
+    	List<Member> members = 
+    			em.createQuery("select m from Member m", Member.class)
+    				.getResultList();
+    	System.out.println("members.size = " + members.size());
+    	
+    	//삭제
+    	em.remove(member);
+    	
+    	
     }
 }
