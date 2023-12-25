b07d88139db3bdaf4e817ea4735a569a6a9ae5c1
gkstjr <gkstjr8332@naver.com>
2023-12-25 11:07:05 +0900

    JPA 예제 공부

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -5,77 +5,48 @@ import java.util.Date;
 import javax.persistence.*;
 
 @Entity
-@Table(name="MEMBER" , uniqueConstraints = {@UniqueConstraint(
-		name = "NAME_AGE_UNIQUE",
-		columnNames = {"NAME","AGE"} )})
 public class Member {
+	
 	@Id
-	@Column(name = "ID")
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 생성전력을 데이터베이스에 위임한다!
-	private String id;
+	@GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
 	
-	@Column(name = "NAME" , nullable = false , length = 10)
-	private String username;
-	
-	private Integer age;
-	
-	@Enumerated(EnumType.STRING) 
-	private RoleType roleType;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date lastModifiedDate;
-	
-	@Lob
-	private String description;
+	private String name;
 	
 	
-	public RoleType getRoleType() {
-		return roleType;
-	}
-	public void setRoleType(RoleType roleType) {
-		this.roleType = roleType;
-	}
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-	public Date getLastModifiedDate() {
-		return lastModifiedDate;
-	}
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = lastModifiedDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public String getId() {
+	private String city;
+	private String strret;
+	private String zipcode;
+	public Long getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getUsername() {
-		return username;
+	public String getName() {
+		return name;
 	}
-	public void setUsername(String username) {
-		this.username = username;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public Integer getAge() {
-		return age;
+	public String getCity() {
+		return city;
 	}
-	public void setAge(Integer age) {
-		this.age = age;
+	public void setCity(String city) {
+		this.city = city;
 	}
-	
-	
+	public String getStrret() {
+		return strret;
+	}
+	public void setStrret(String strret) {
+		this.strret = strret;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
 	
 }
