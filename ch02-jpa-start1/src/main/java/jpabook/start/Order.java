5280e9d60e37cac8f5aa20cf3f4cda67034bbffd
gkstjr <gkstjr8332@naver.com>
2023-12-27 01:27:30 +0900

    영속성 전이

--- ch02-jpa-start1/src/main/java/jpabook/start/Order.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Order.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -28,14 +30,14 @@ public class Order extends BaseEntity {
 	@Column(name =  "ORDER_ID")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "MEMBER_ID")
 	private Member member;
 	
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 	
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL ,fetch = FetchType.LAZY)
 	@JoinColumn(name = "DELIVERY_ID")
 	private Delivery delivery;
 	
