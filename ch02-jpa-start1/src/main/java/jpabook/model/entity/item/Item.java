699c17b775fe4c9dc75e3efb6a08208b576c79cd
gkstjr <gkstjr8332@naver.com>
2023-12-26 12:41:16 +0900

    상속 매핑과 @maappedSuperclass 사용법

--- ch02-jpa-start1/src/main/java/jpabook/model/entity/item/Item.java
+++ ch02-jpa-start1/src/main/java/jpabook/model/entity/item/Item.java
@@ -1,17 +1,20 @@
-package jpabook.start;
+package jpabook.model.entity.item;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.*;
 
+import jpabook.start.Category;
+
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Item {
 
 	@Id @GeneratedValue
 	@Column(name = "ITEM_ID")
 	private Long id;
-	
 	
 	private String name;
 	private int price;
