d409747aa4ba27a2ad82479d8b3b269093ea2bce
gkstjr <gkstjr8332@naver.com>
2024-03-20 13:09:49 +0900

    프로그래머스 시저 암호 문제풀이

--- algo/src/Solution.java
+++ algo/src/Solution.java
@@ -1,18 +1,26 @@
 class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
-        int maxW = Integer.MIN_VALUE;
-        int maxH = Integer.MIN_VALUE;
+    public String solution(String s, int n) {
+        String answer = "";
+        char[] arr = s.toCharArray();
         
-        for(int i = 0; i < sizes.length; i++) {
-            int w = Math.max(sizes[i][0] , sizes[i][1]);
-            int h = Math.min(sizes[i][0] , sizes[i][1]);
-                       
-            maxW = (w > maxW) ? w : maxW;
-            maxH = (h > maxH) ? h : maxH;
+        for(int i = 0; i < arr.length; i++) {
+            char ch = arr[i];
+            if(Character.isLowerCase(ch)) { //소문자
+                if((ch - 0) + n > 122) { //z를 넘을 때
+                   answer += (char)(((ch - 0) + n - 122) + 96);
+                }else {
+                    answer += (char)(ch - 0 + n);
+                } 
+            }else if(Character.isUpperCase(ch)) { //대문자일 때
+                 if((ch - 0) + n > 90) { //Z를 넘을 때
+                   answer += (char)(((ch - 0) + n - 90) + 64);
+                }else {
+                    answer += (char)(ch - 0 + n);
+                } 
+            }else {//공백
+                answer += " ";
+            }
         }
-        
-        
-        return maxW * maxH;
+        return answer;
     }
 }
