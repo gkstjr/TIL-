f758e9ffbf795ae90bbf3cb4d241e55b6ee4922c
gkstjr <gkstjr8332@naver.com>
2024-03-22 06:46:09 +0900

    자바의 신 정리

---
+++ algo/src/got_of_java/Calculator.java
@@ -0,0 +1,16 @@
+package got_of_java;
+
+public class Calculator {
+	
+	public int subtrackt(int num1 , int num2) {
+		return num1 - num2;
+	}
+	
+	public int multiply(int num1 , int num2) {
+		return num1 * num2;
+	}
+	
+	public int divide(int num1 , int num2) {
+		return num1 / num2;
+	}
+}
