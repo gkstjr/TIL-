3dfa3d51bb2666dc1bf717dc557eaf8d934e4c72
gkstjr <gkstjr8332@naver.com>
2024-03-22 07:29:16 +0900

    자바의 신 정리

--- algo/src/got_of_java/Profile.java
+++ algo/src/got_of_java/Profile.java
@@ -1,7 +1,32 @@
 package got_of_java;
 
 public class Profile {
+	String name;
+	int age;
+	
 	public static void main(String[] args) {
-		System.out.println("김한석");
+		Profile profile = new Profile();
+		profile.setName("Min");
+		profile.setAge(20);
+		profile.printName();
+		profile.printAge();
 	}
+	
+	public void setName(String str) {
+		this.name = str;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void printName() {
+		System.out.println(name);
+	}
+	
+	public void printAge() {
+		System.out.println(age);
+	}
+	
+	
 }
