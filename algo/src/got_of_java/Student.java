bec49ba9dbeaa686397776ea47a8efe902052604
gkstjr <gkstjr8332@naver.com>
2024-03-26 20:10:32 +0900

    자바의 신 직접해봅시다.

---
+++ algo/src/got_of_java/Student.java
@@ -0,0 +1,24 @@
+package got_of_java;
+
+public class Student {
+	public String name;
+	public String address;
+	public String phone;
+	public String email;
+
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public Student(String name, String address , String phone , String email) {
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public String toString() {
+		return  name + " " + address + " " + phone + " " + email;
+	}
+}
