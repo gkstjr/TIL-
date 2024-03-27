283eaaa69aa71f345e06c85e070e628212b6f644
gkstjr <gkstjr8332@naver.com>
2024-03-27 16:08:06 +0900

    StringBuilder 사용해서 문제 풀이

--- algo/src/Solution.java
+++ algo/src/Solution.java
@@ -1,25 +1,18 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         //왼쪽 출발
         for(int i = 1; i < food.length; i++) {
             int cnt = food[i] / 2;
             for(int j = 0; j < cnt; j++) {
-                answer += String.valueOf(i);
+                sb.append(String.valueOf(i));
             }
         }
+                
+        answer = sb.toString() + "0" + sb.reverse().toString();
         
-        answer += "0";
-        
-        //오른쪽 출발
-        for(int i = food.length - 1; i > 0; i--) {
-            int cnt = food[i] / 2;
-            for(int j = 0; j <cnt; j++) {
-                answer += String.valueOf(i);
-            }
-        }
-       // System.out.println(answer);
         return answer;
     }
 }
