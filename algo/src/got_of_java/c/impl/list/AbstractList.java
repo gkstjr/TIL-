956a27420f15d4a17e1c969405e842a608b44e5c
gkstjr <gkstjr8332@naver.com>
2024-03-29 18:04:31 +0900

    자바의 신 직접 해봅시다(enum , 추상클래스 , 인터페이스)

---
+++ algo/src/got_of_java/c/impl/list/AbstractList.java
@@ -0,0 +1,7 @@
+package got_of_java.c.impl.list;
+
+public abstract class AbstractList implements List{
+	
+	public abstract void clear();
+		
+}
---
+++ algo/src/got_of_java/c/impl/list/List.java
@@ -0,0 +1,12 @@
+package got_of_java.c.impl.list;
+
+public interface List {
+	
+	public void add();
+
+	public void update(int index , Object value);
+	
+	public void remove(int index);
+	
+	public void get(int index);
+}
