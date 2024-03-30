2dc0b7c516c2e01dbd96de4fd089523539c64545
gkstjr <gkstjr8332@naver.com>
2024-03-30 16:17:35 +0900

    자바의 신 직접해봅시다(내부 클래스)

---
+++ algo/src/got_of_java/c/inner/KeyEventListener.java
@@ -0,0 +1,7 @@
+package got_of_java.c.inner;
+
+public interface KeyEventListener {
+	
+	public void onKeyDown();
+	public void onKeyUp();
+}
---
+++ algo/src/got_of_java/c/inner/MyPage.java
@@ -0,0 +1,31 @@
+package got_of_java.c.inner;
+
+public class MyPage {
+	public InputBox input;
+	
+	public static void main(String[] args) {
+		MyPage myPage = new MyPage();
+		myPage.setUI();
+		myPage.pressKey();
+	}
+	
+	public void setUI() {
+		input = new InputBox();
+		
+		input.setKeyListener(new KeyEventListener() {
+			
+			public void onKeyDown() {
+				System.out.println("Key Down");
+			};
+			
+			public void onKeyUp() {
+				System.out.println("Key Up");
+			}
+		});
+	}
+	
+	public void pressKey() {
+		input.listenerCalled(input.KEY_UP);
+		input.listenerCalled(input.KEY_DOWN);
+	}
+}
