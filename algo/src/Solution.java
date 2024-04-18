7b27f804d20bb237a8ba58ed00b6bfb000f016f3
gkstjr <gkstjr8332@naver.com>
2024-04-18 21:08:11 +0900

    2차원 배열 프로그래머스 문제

--- algo/src/Solution.java
+++ algo/src/Solution.java
@@ -1,12 +1,23 @@
 import java.util.ArrayList;
 
 class Solution {
-	public int[] solution(int start, int end) {
-		ArrayList<Integer> answer = new ArrayList<Integer>();
-		
-		for(int i = start; i <= end; i++) {
-			answer.add(i);
-		}
-		return answer.stream().mapToInt(i -> i).toArray();
+	public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        
+        for(int i = 0 ; i < queries.length; i++) {
+        	int s = queries[i][0];
+        	int e = queries[i][1];
+        	int k = queries[i][2];
+        	int min = Integer.MAX_VALUE;
+        	for(int j = s; j <= e; j++) {
+        		if((min > arr[j]) && arr[j] > k) {
+        			min = arr[j];
+        		}
+        	}
+        	if(min > 2000000) min = -1;
+        	answer[i] = min;
+        	System.out.println(min);
+        }
+        return answer;
     }
 }
