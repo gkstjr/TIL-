713faf68591d16db027644818b1eb6f0a384a884
gkstjr <gkstjr8332@naver.com>
2024-04-22 06:50:52 +0900

    콜럼버스(코딩테스트 스터디) 0일차 몸풀기

---
+++ src/colombus/day0_1.java
@@ -0,0 +1,10 @@
+package colombus;
+
+public class day0_1 {
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello World!");
+		System.out.println(sb);
+	}
+}
---
+++ src/colombus/day0_2.java
@@ -0,0 +1,18 @@
+package colombus;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class day0_2 {
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.valueOf(st.nextToken());
+		int sum = A + B;
+		System.out.println(sum);
+	
+	}
+}
