549557984df3cd9a205885ea889e70cb06896a09
gkstjr <gkstjr8332@naver.com>
2024-04-22 17:56:30 +0900

    콜럼버스 스터디 1일차

---
+++ src/colombus/day1_basic.java
@@ -0,0 +1,22 @@
+package colombus;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class day1_basic {
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.valueOf(br.readLine());
+		int[] num = new int[N + 1];
+		
+		if(N != 0)  num[1] = 1;
+		
+		for(int i = 2 ; i <= N; i++ ) {
+			num[i] = num[i - 1] + num[i - 2];
+		}
+		
+		System.out.println(num[N]);
+		
+	}
+}
