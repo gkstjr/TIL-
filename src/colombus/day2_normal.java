90d4bc425252e63dcbe701fb3ae0f2ed2129a873
gkstjr <gkstjr8332@naver.com>
2024-04-23 15:53:15 +0900

    콜럼버스 스터디 2일 차 완료

--- src/colombus/day2_normal.java
+++ src/colombus/day2_normal.java
@@ -2,7 +2,7 @@ package colombus;
 
 import java.io.*;
 import java.util.StringTokenizer;
-public class Main {
+public class day2_normal {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
