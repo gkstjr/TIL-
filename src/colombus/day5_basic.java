f5d1c4f2526594570c73cc06f97b5a8ba8759996
gkstjr <gkstjr8332@naver.com>
2024-04-26 22:45:21 +0900

    콜럼버스 5일차

---
+++ src/colombus/day5_basic.java
@@ -0,0 +1,46 @@
+package colombus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+ 
+public class day5_basic {	
+	public static int n, k;
+	public static int[] card;
+	public static boolean[] visited;
+	public static HashSet<String> hashset = new HashSet<>();
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	n = Integer.parseInt(st.nextToken()); 
+    	st = new StringTokenizer(br.readLine());
+    	k = Integer.parseInt(st.nextToken());
+    
+    	card = new int[n];
+    	visited = new boolean[n];
+    	
+    	for(int i=0;i<n;i++) {
+    		st = new StringTokenizer(br.readLine());
+    		card[i] = Integer.parseInt(st.nextToken()); 
+    	}
+    	
+    	simulate(new String(), 0);
+    	System.out.println(hashset.size());
+	}
+    
+    public static void simulate(String s, int level){
+    	if(level == k) {
+//    		System.out.println(s);
+    		hashset.add(s);
+    		return ;
+    	}
+    	for(int i=0;i<n;i++) {
+    		if(visited[i] == true) continue;
+    		visited[i] = true;
+    		simulate( s + String.valueOf(card[i]), level + 1);
+    		visited[i] = false;
+    	}	
+    }
+}
