840cd412ab94ec732dd700d9d61b62ebf1a883f4
gkstjr <gkstjr8332@naver.com>
2024-04-30 15:57:02 +0900

    소스 수정

--- src/colombus/week2_dfs_bfs_graph/graph_6603.java
+++ src/colombus/week2_dfs_bfs_graph/graph_6603.java
@@ -12,29 +12,50 @@ import java.util.*;
 public class graph_6603 {
 	static int k;
 	static int[] S;
-	static StringBuilder sb;
+	static int[] ans;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		while(true) {
-			sb = new StringBuilder();
 			st = new StringTokenizer(br.readLine());
 			
 			k = Integer.valueOf(st.nextToken());
 			S = new int[k];
+			ans = new int[6];
+			visited = new boolean[k];
+			
 			if(k == 0) break;
 			for(int i = 0; i < k; i++) {
 				S[i] = Integer.valueOf(st.nextToken());
 			}
 			
 			dfs(0 , 0);
-			
-			System.out.println(sb);
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 	
 	public static void dfs(int start , int depth) {
+		if(depth == 6) {
+			for(int i = 0 ; i < ans.length; i++) {
+				sb.append(ans[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i < S.length; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				ans[depth] = S[i];
+				dfs(i, depth + 1);
+				visited[i] = false;
+			}
+		}
+		
 		
 	}
 }
