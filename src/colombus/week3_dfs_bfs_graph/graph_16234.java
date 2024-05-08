22c0d8ce0f7e876493b7c9cf51bbb434a7e9816d
gkstjr <gkstjr8332@naver.com>
2024-05-08 17:56:50 +0900

    코딩테스트 준비

---
+++ src/colombus/week3_dfs_bfs_graph/graph_16234.java
@@ -0,0 +1,18 @@
+package colombus.week3_dfs_bfs_graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//문제 설명 : 인구 이동 문제
+//핵심 개념 : bfs 알고리즘
+//예상 시간복잡도 : 
+//오답노트 -> 
+public class graph_16234 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
+	
