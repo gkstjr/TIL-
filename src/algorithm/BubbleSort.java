68ff60220b5845f812af065d9a7e9dc0079cb4da
gkstjr <gkstjr8332@naver.com>
2024-05-15 16:04:46 +0900

    알고리즘 공부(거품 정렬 개념)

--- src/algorithm/BubbleSort.java
+++ src/algorithm/BubbleSort.java
@@ -1,13 +1,23 @@
 package algorithm;
-
+//거품 정렬(BubbleSort) 시간복잡도 = o(n^2)
 public class BubbleSort {
 	static int[] arr = new int[40];
 	public static void main(String[] args) {
 		insertData();
-		for(int i : arr) {
-			System.out.println(i);
+		int temp = 0;
+		for(int i = 0 ; i < arr.length - 1; i++) {
+			for(int j = 1; j < arr.length - i; j++) {
+				if(arr[j] > arr[j - 1]) { //앞의 값이 더 크면 큰 값을 뒤로 가게 변경해주기
+					temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
+				}
+			}
 		}
+		
+		printArray();
 	}
+	
 	
 	
 	static void insertData() {
@@ -18,4 +28,15 @@ public class BubbleSort {
 		}
 	
 	}
+
+	static void printArray() {
+		System.out.print("[ ");
+		for(int i : arr) {
+			System.out.print(i + " , ");
+		}
+		
+		System.out.print("]");
+
+	}
+	
 }
