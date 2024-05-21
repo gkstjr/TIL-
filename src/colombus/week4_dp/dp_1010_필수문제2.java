54c8df32967efdb849cca1c9faab63b1f26194d3
gkstjr <gkstjr8332@naver.com>
2024-05-21 15:28:45 +0900

    해결

--- src/colombus/week4_dp/dp_1010_필수문제2.java
+++ src/colombus/week4_dp/dp_1010_필수문제2.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 //문제 설명 : 파보나치 수 2
-//핵심 개념 : dp 알고리즘 / 수학 조합의 성질 n+1Cr+1 =  nCr + nCr+1 
+//핵심 개념 : dp 알고리즘 / 수학 조합의 성질 n+1Cr+1 =  nCr + nCr+1 (n == r 이면 1 또는 r 이 0이면 1)
 //예상 시간복잡도 : 
 //오답노트 -> 
 public class dp_1010_필수문제2 {
