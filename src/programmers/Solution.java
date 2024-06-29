3bf3841ab9cd96361d5a98dcf46f18d14f463244
gkstjr <gkstjr8332@naver.com>
2024-06-29 22:16:01 +0900

    정

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,18 +1,7 @@
 package programmers;
 
 import java.util.*;
-<<<<<<< HEAD
-class Solution {
-    public List<Integer> solution(int[] answers) {
-    	
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        int cnt1 = 0; int cnt2 = 0; int cnt3 = 0;
-        for(int i = 0 ; i < answers.length; i++) {
-          cnt1 += one(i , answers[i]);
-          cnt2 += two(i , answers[i]);
-          cnt3 += three(i , answers[i]);
 
-=======
 class Solution { //프로그래머스 재seting
     public int solution(int k, int m, int[] score) {
         int answer = 0;
@@ -20,9 +9,8 @@ class Solution { //프로그래머스 재seting
         
         for(int i = score.length - m; i >= 0; i-= m) {
             answer += score[i] * m;
->>>>>>> branch 'master' of https://github.com/gkstjr/TIL-.git
         }
         
         return answer;
     }
-}
+} 
