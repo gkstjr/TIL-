9d88251baf1249ea525939c716c787ee332cbf8d
gkstjr <gkstjr8332@naver.com>
2024-07-05 22:25:11 +0900

    7월 리마인드.

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -2,7 +2,7 @@ package programmers;
 
 import java.util.*;
 
-class Solution { //프로그래머스 재seting
+class Solution { //1.이력서 . 포트폴리오 update 2. CS전공 핵심요약집 3. 코딩 테스트(프래그래머스)  4. FeedB 프로젝트 이해
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
