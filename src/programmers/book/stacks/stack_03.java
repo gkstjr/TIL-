7e06fdba0bca9f628026c62493111d992ce9d496
gkstjr <gkstjr8332@naver.com>
2024-08-03 17:02:47 +0900

    스택 문제 04 풀이 완료(오호 level 2도 쉬운데? 자만하지 말자)

--- src/programmers/book/stacks/stack_03.java
+++ src/programmers/book/stacks/stack_03.java
@@ -7,49 +7,21 @@ public class stack_03 {
 		
 	}
 
-	 public int solution(String s) {
-	        int answer = 0;
-	  
-	        char[] ch = s.toCharArray();
-	        ArrayList<Character> arr = new ArrayList<Character>();
+	 public int solution(String s)
+	    {
+	        int answer = -1;
 	        
-	        for(char c : ch) {
-	            arr.add(c);
+	        char[] ch = s.toCharArray();
+	        Stack<Character> stk = new Stack();
+	        
+	        for(int i = 0 ; i < ch.length; i++) {
+	            char c = ch[i];
+	            
+	            if(!stk.isEmpty() && stk.peek() == c) {
+	                stk.pop();
+	            }else stk.push(c);
 	        }
-	        Stack<Character> stk = new Stack<Character>(); 
-	       
-	        Loop1 :
-	        for(int i = 0 ; i < s.length(); i++) {
-	            char first = arr.get(0);
-	            for(int j = 0; j < s.length(); j++) { 
-	                char a = arr.get(j);
-	                if(a == '[') stk.push('[');
-	                else if(a == '{') stk.push('{');
-	                else if(a == '(') stk.push('(');
-	                else if(a == ']') {
-	                    if(stk.isEmpty() || stk.peek() != '[') {
-	                        arr.remove(0);
-	                        arr.add(first);
-	                        continue Loop1;    
-	                    }else stk.pop();
-	                }else if(a == '}') {
-	                    if(stk.isEmpty() || stk.peek() != '{') {
-	                        arr.remove(0);
-	                        arr.add(first);
-	                        continue Loop1;    
-	                    }else stk.pop();
-	                }else if(a == ')') {
-	                     if(stk.isEmpty() || stk.peek() != '(') {
-	                        arr.remove(0);
-	                        arr.add(first);
-	                        continue Loop1;    
-	                    }else stk.pop();
-	                }
-	            }
-	            if(stk.isEmpty()) answer++;
-	            stk.clear(); 
-	            arr.remove(0); arr.add(first);
-	        }
-	        return answer;
+
+	        return stk.isEmpty() ? 1 : 0;
 	    }
 }
---
+++ src/programmers/book/stacks/stack_04.java
@@ -0,0 +1,21 @@
+package programmers.book.stacks;
+import java.util.*;
+
+public class stack_04 {
+	public static void main(String[] args) {
+		Stack<Integer> stc = new Stack<Integer>();
+		
+		stc.push(1);
+		stc.push(3);
+		
+		System.out.println(stc.isEmpty());
+		System.out.println(stc.pop());
+		
+	}
+
+	public static int[] solution(int[] arr) {
+		
+		
+		return arr;
+	}
+}
