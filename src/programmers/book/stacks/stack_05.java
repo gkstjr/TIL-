36d879fdc212fb46690bd7e1bd9ee44688439e29
gkstjr <gkstjr8332@naver.com>
2024-08-03 18:00:26 +0900

    스택 문제풀이(주식 가격 level2)

---
+++ src/programmers/book/stacks/stack_05.java
@@ -0,0 +1,45 @@
+package programmers.book.stacks;
+import java.util.*;
+
+public class stack_05 {
+	public static void main(String[] args) {
+		Stack<Integer> stc = new Stack<Integer>();
+		
+		stc.push(1);
+		stc.push(3);
+		
+		System.out.println(stc.isEmpty());
+		System.out.println(stc.pop());
+		
+	}
+
+	  public int[] solution(int[] prices) {
+	        Stack<Integer> stk = new Stack<Integer>();
+
+	        int size = prices.length;
+	        int[] answer = new int[size];
+
+	        for(int i = 0 ; i < size; i++) {
+	            int number = prices[i];
+	            if(stk.isEmpty()) {
+	                stk.push(i);
+	                continue;
+	            }else if(prices[stk.peek()] > number) {
+	                while(!stk.isEmpty() && prices[stk.peek()] > number) {
+	                    int pop = stk.pop();
+	                    answer[pop] = i - pop;
+	                }
+	                stk.push(i);
+
+	            }else {
+	                stk.push(i);
+	            }
+	        }
+	        
+	        while(!stk.isEmpty()) {
+	            int pop = stk.pop();
+	            answer[pop] = size - 1 - pop;
+	        }
+	        return answer;
+	    }
+}
