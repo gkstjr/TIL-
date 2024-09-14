14d2ea62a69a323470bdeb34d1a7af5dbb0968a2
gkstjr <gkstjr8332@naver.com>
2024-09-14 13:07:38 +0900

    Queue 시작

---
+++ src/programmers/book/queue/TEST.java
@@ -0,0 +1,21 @@
+package programmers.book.queue;
+import java.util.*;
+
+public class TEST {
+	public static void main(String[] args) {
+		Stack<Integer> stc = new Stack<Integer>();
+		
+		stc.push(1);
+		stc.push(3);
+		
+		System.out.println(stc.isEmpty());
+		System.out.println(stc.pop());
+		
+	}
+
+	public static int[] solution(int[] arr) {
+		
+		
+		return arr;
+	}
+}
