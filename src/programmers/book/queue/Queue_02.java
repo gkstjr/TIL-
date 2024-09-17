c50de7cd68f9bd6359f3780c760a79be6e04581a
gkstjr <gkstjr8332@naver.com>
2024-09-17 23:36:03 +0900

    큐 2번째 문제 준비

---
+++ src/programmers/book/queue/Queue_02.java
@@ -0,0 +1,15 @@
+package programmers.book.queue;
+import java.util.*;
+
+public class Queue_02 {
+	public static void main(String[] args) {
+		
+		
+	}
+
+	public static int[] solution(int[] arr) {
+		
+		
+		return arr;
+	}
+}
