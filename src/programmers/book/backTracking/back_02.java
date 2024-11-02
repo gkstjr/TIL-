0811c65df1b6bfd4c1775298a6541c8521019df3
gkstjr <gkstjr8332@naver.com>
2024-11-02 17:36:53 +0900

    백트래킹 2번 문제풀이

---
+++ src/programmers/book/backTracking/back_02.java
@@ -0,0 +1,43 @@
+package programmers.book.backTracking;
+import java.util.*;
+
+public class back_02 {
+	   private static int[] arr; // index가 열 , 값이 행
+	    private static int N;
+	    private static int answer;
+	    public int solution(int n) {
+	        answer = 0;
+	        arr = new int[n];
+	        N = n;
+	        
+	        back(0);
+	        return answer;
+	    }
+	    
+	    public static void back(int col) {
+	        if(col == N) {
+	            answer += 1;
+	            return;
+	        }
+	        
+	        for(int i = 0; i < N; i++) {
+	            arr[col] = i;
+	            
+	            if(isPossible(col)) {
+	                back(col + 1);
+	            }
+	        }
+	    }
+	    
+	    private static boolean isPossible(int col) {
+	        for(int i = 0 ; i < col; i++) {
+	            if(arr[col] == arr[i]) return false;
+	        }
+	        
+	        
+	        for(int i = 0 ; i < col; i++ ) {
+	            if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false;
+	        }
+	        return true;
+	    }
+}
