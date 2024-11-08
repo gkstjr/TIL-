196b1f675ab66d053e9a5076ebd1c70e8773809d
gkstjr <gkstjr8332@naver.com>
2024-11-08 11:28:26 +0900

    정렬 문제 풀이 완료

---
+++ src/programmers/book/sort/Sort_06.java
@@ -0,0 +1,15 @@
+package programmers.book.sort;
+import java.util.*;
+
+public class Sort_06 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i = 1 ;i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i - 1])) return false;
+        }
+        return true;
+    }
+}
