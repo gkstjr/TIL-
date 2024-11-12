5a5965d5c04a027115315cc2b397e07edb382464
gkstjr <gkstjr8332@naver.com>
2024-11-12 22:57:53 +0900

    행렬 곱셈 후 전치행렬 문제풀이

--- src/programmers/book/simulation/simul_basic2.java
+++ src/programmers/book/simulation/simul_basic2.java
@@ -3,29 +3,32 @@ import java.util.*;
 
 public class simul_basic2 {
 	public static void main(String[] args) {
+//		int[][] matrix1 = {{1,2,3},{4,5,6},{7,8,9}};
+//		int[][] matrix2 = {{9,8,7},{6,5,4},{3,2,1}};
+		int[][] matrix1 = {{2,4,6},{1,3,5},{7,8,9}};
+		int[][] matrix2 = {{9,1,2},{4,5,6},{7,3,8}};
 		
-		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-		int M = arr.length;
-		int N = 2;
-		int[][] answer = new int[M][M];
-		for(int k = 0 ; k < N; k++) {
-			for(int i = 0 ; i < M ; i++) {
-				for(int j = 0 ; j < M; j++) {
-					answer[j][M - i - 1] = arr[i][j];
-				}
+		int[][] answer = new int[3][3];
+		for(int i = 0; i < matrix1.length; i++) {
+			for(int j = 0 ; j < matrix1.length; j++) {
+				answer[i][j] += matrix1[i][0] * matrix2[0][j];
+				answer[i][j] += matrix1[i][1] * matrix2[1][j];
+				answer[i][j] += matrix1[i][2] * matrix2[2][j];
 			}
-			
-			arr = answer;
+		}
+		int[][] answer2 = new int[3][3];
+		for(int i = 0 ; i < 3; i++) {
+			for(int j = 0 ; j < 3; j++) {
+				answer2[i][j] = answer[j][i];
+			}
 		}
 		
-		System.out.println("====================");
-		for(int i = 0; i < M ; i++) {
-			for(int j = 0 ; j < M; j++) {
-				System.out.print(answer[i][j] + " ");
+		for(int i = 0 ; i < 3; i++) {
+			for(int j = 0 ; j < 3; j++) {
+				System.out.print(answer2[i][j] + " ");
 			}
 			System.out.println();
 		}
-
 	}
 
 	public static int[] solution(int[] arr) {
