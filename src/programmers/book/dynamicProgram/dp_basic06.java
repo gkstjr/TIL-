828db6326397f0f9273e9c008bb1fd442a0eba84
gkstjr <gkstjr8332@naver.com>
2024-12-12 00:01:50 +0900

    가장 큰 정사각형 찾기 문제 완료.(구현은 힌트 보고 내 힘으로 하긴 했는데 문제를 푸는 방식을 생각해내기 어려웠다.)

--- src/programmers/book/dynamicProgram/dp_basic06.java
+++ src/programmers/book/dynamicProgram/dp_basic06.java
@@ -2,13 +2,41 @@ package programmers.book.dynamicProgram;
 import java.util.*;
 
 public class dp_basic06 {
-	  public int solution(int [][]board)
+	   static int[] mx = {-1 , -1 , 0}; //열
+	    static int[] my = {0 , -1 , -1}; //행
+	    
+	    public int solution(int [][]board)
 	    {
-	        int answer = 1234;
-
-	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-	        System.out.println("Hello Java");
-
-	        return answer;
+	        int answer = Integer.MIN_VALUE;
+	        
+	        int rowSize = board.length; //행 크기
+	        int colSize = board[0].length; //열 크기
+	        int[][] dp = new int[rowSize][colSize];
+	        
+	        for(int i = 0; i < rowSize; i++) {
+	            Loop1:
+	            for(int j = 0 ; j < colSize; j++) {
+	                if(board[i][j] != 1) continue;     
+	                int min = Integer.MAX_VALUE;
+	                for(int k = 0; k < 3; k++) {
+	                  int nextX = j + mx[k];
+	                  int nextY = i + my[k];
+	                    
+	                  if(nextX < 0 || nextY < 0) {
+	                      dp[i][j] = 1;
+	                      continue Loop1;
+	                  }
+	                  min = Math.min(min, dp[nextY][nextX]);
+	                }
+	                dp[i][j] = min + 1;
+	            }
+	        }
+	        
+	        for(int i = 0; i < rowSize; i++) {
+	            for(int j = 0 ; j < colSize; j++) {
+	                answer = Math.max(answer , dp[i][j]);
+	            }
+	        }
+	        return answer * answer;
 	    }
 }
