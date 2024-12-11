4106413ef53441dafc358610d608fc86f6f91520
gkstjr <gkstjr8332@naver.com>
2024-12-11 23:43:48 +0900

    dp 가장 큰 정사각형 찾기(힌트 보고 함..)

---
+++ src/programmers/book/dynamicProgram/dp_basic06.java
@@ -0,0 +1,14 @@
+package programmers.book.dynamicProgram;
+import java.util.*;
+
+public class dp_basic06 {
+	  public int solution(int [][]board)
+	    {
+	        int answer = 1234;
+
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	        System.out.println("Hello Java");
+
+	        return answer;
+	    }
+}
