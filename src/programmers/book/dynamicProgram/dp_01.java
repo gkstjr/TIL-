7710a0bcd81f07c0caba5f4477ee449a107d63f1
gkstjr <gkstjr8332@naver.com>
2024-12-12 15:56:29 +0900

    그리디 이론 공부 후 문제 풀이

--- src/programmers/book/dynamicProgram/dp_01.java
+++ src/programmers/book/dynamicProgram/dp_01.java
@@ -9,8 +9,8 @@ public class dp_01 {
 	        dp[1] = 1;
 	        for(int i = 2; i <= n; i++) {
 	            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
-	        }
-	        
+	        }      
+	       
 	        return dp[n];
 	    }
 }
