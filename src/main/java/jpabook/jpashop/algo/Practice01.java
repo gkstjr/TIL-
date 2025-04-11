dd92e6c54ec83c6f57d0306a8307bc6933b97a8d
gkstjr <gkstjr8332@naver.com>
2025-04-12 00:00:03 +0900

    배열 연습문제 2

---
+++ src/main/java/jpabook/jpashop/algo/Practice01.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.algo;
+
+import java.util.*;
+
+class Practice01 {
+    public static void main(String[] args) {
+        solution(new int[]{1, 2, 3, 4});
+    }
+
+    public static int[] solution(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+
+    }
+}
---
+++ src/main/java/jpabook/jpashop/algo/hubDnC01.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.algo;
+
+import java.util.HashMap;
+
+public class hubDnC01 {
+
+
+    public String solution(String[] arr) {
+
+        HashMap<String , String> map = new HashMap<String, String>();
+        map.put("1","0"); map.put("3","0"); map.put("5","0");
+
+        for(int i = 0; i < arr.length; i++) {
+            int answer = arr[i].charAt(9);
+            int score = arr[i].charAt(20);
+
+
+        }
+    }
+}
